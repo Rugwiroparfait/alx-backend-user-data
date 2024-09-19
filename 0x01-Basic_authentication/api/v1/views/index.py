6383615ecd8b8cc0f2437a13fb4eec5e686a3779
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-09-19 21:57:15 +0000

    Error handler Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route("unauthorized", strict_slashes=False)
+def unauthorized_test():
+    """ test 401 unauthorized errror """
+    abort(401)
