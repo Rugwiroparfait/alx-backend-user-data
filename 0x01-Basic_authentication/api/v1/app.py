fb6aa83ef8eb063c99c175c35e531fefddb3ff52
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-09-20 21:40:41 +0200

    pycodestyle

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -50,7 +50,7 @@ def before_request_func():
     if auth is None:
         return
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                                  '/api/v1/forbidden/']
+                      '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
@@ -58,6 +58,7 @@ def before_request_func():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
