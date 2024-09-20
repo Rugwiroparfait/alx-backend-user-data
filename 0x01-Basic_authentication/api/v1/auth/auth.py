54059062b9b140342811c69525aff544104c0045
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-09-20 12:58:14 +0000

    Unauthorized

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,9 +21,9 @@ class Auth:
         """ Implement request validation and ensure that/
             only authenticated users can access certain routes.
         """
-        if request is None or 'Authorization' not in request.headers:
+        if request is None:
             return None
-        return request.headers['Authorization']
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Return None. """
