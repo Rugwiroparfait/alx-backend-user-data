a959bab869732a8d8af5c4ae6b504d8021532ccf
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-09-21 21:29:55 +0000

    SessionAuth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -4,6 +4,8 @@ Route module for the API
 """
 from os import getenv
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.auth import Auth
+from api.v1.auth.session_auth import SessionAuth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -21,6 +23,9 @@ auth_type = os.getenv("AUTH_TYPE")
 if auth_type == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module of session AUthenntication
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Session authentication class that inherits from Auth """
+    pass
