bfbb98341ece80ac0a5b37dc8b76395a2a95e92a
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-09-21 22:48:23 +0000

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -59,12 +59,11 @@ def before_request_func():
                       '/api/v1/forbidden/','/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None and auth.session_cookie(request):
-        print(f"{auth.authorization_header(request)}")
+    if auth.authorization_header(request) is None and\
+            auth.session_cookie(request):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-
     request.current_user = auth.current_user(request)
 
 
