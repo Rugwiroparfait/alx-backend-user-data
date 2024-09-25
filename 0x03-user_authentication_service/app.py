3676160c4d46976384354e271511a26f32c081c1
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-09-25 16:27:28 +0200

    app (registering a user)

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,9 +3,26 @@
 Basic Flask app
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/users', methods=['POST'])
+def register_user():
+    """Register a new user route"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        # registering a user
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        # If the user already exists, return a 400 error
+        return jsonify({"message": "email already registered"}), 400
 
 
 @app.route("/")
@@ -16,4 +33,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
