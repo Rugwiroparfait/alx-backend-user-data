099996c11ac5f3f39ad3f83fd6f71532d9c94cd1
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-09-27 00:05:53 +0200

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,3 +72,23 @@ class Auth:
     def _generate_uuid(self) -> str:
         """generate a new uuid and returns its string represantation."""
         return str(uuid.uuid4())
+
+    def create_session(self, email: str) -> str:
+        """Creates a session ID for the user and stores it in the database.
+
+        Args:
+            email (str): The user's email.
+
+        Returns:
+            str: The session ID if the user is found, otherwise None.
+        """
+        try:
+            # Locate the user by email
+            user = self._db.find_user_by(email=email)
+            # Generate a new session ID using the _generate_uuid method
+            session_id = self._generate_uuid()
+            # Update the user's session ID in the database
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
